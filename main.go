b4fbd6652bb2bfaf26645ebb79e6e27e11e688af
Chitoku <chitoku-k@users.noreply.github.com>
2020-08-27 23:53:48 +0900

    Use fmt.Errorf for wrapping errors (#23)

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/chitoku-k/form-to-slack/application/server"
 	"github.com/chitoku-k/form-to-slack/infrastructure/config"
 	"github.com/chitoku-k/form-to-slack/infrastructure/slack"
 	"github.com/chitoku-k/form-to-slack/service"
 	"github.com/dpapathanasiou/go-recaptcha"
-	"github.com/pkg/errors"
 )
 
 func main() {
 	env, err := config.Get()
 	if err != nil {
-		panic(errors.Wrap(err, "failed to initialize config"))
+		panic(fmt.Errorf("failed to initialize config: %w", err))
 	}
 
 	recaptcha.Init(env.ReCaptchaSecret)
