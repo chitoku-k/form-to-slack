b149a688d2b77b80279c155bbef730a00ade35a8
Chitoku <odango@chitoku.jp>
2020-10-05 23:56:34 +0900

    Support graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/chitoku-k/form-to-slack/application/server"
 	"github.com/chitoku-k/form-to-slack/infrastructure/config"
 	"github.com/chitoku-k/form-to-slack/infrastructure/slack"
@@ -10,6 +15,16 @@ import (
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		defer close(sig)
+		<-sig
+		cancel()
+	}()
+
 	env, err := config.Get()
 	if err != nil {
 		logrus.Fatalf("Failed to initialize config: %v", err)
@@ -18,6 +33,9 @@ func main() {
 	recaptcha.Init(env.ReCaptchaSecret)
 	slackNotifier := slack.NewSlackNotifier(env)
 	slackService := service.NewSlackService(slackNotifier)
-	engine := server.NewEngine(env, slackService)
-	engine.Start()
+	engine := server.NewEngine(env.Port, env.AllowedOrigins, slackService)
+	err = engine.Start(ctx)
+	if err != nil {
+		logrus.Fatalf("Failed to start web server: %v", err)
+	}
 }
